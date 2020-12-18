772b4f69de76f5d67d2d4474eabbf7a7aac96115
freshfeng3017 <feng1012221566@gmail.com>
2020-12-18 11:06:12 +0800

    sp01通用代码

--- springcloud1/sp01-commons/src/main/java/cn/tedu/sp01/service/OrderService.java
+++ springcloud1/sp01-commons/src/main/java/cn/tedu/sp01/service/OrderService.java
@@ -3,6 +3,16 @@ package cn.tedu.sp01.service;
 import cn.tedu.sp01.pojo.Order;
 
 public interface OrderService {
+    /**
+     * 获取订单
+     * @param orderId 订单ID
+     * @return 返回Order对象
+     */
     Order getOrder(String orderId);
+
+    /**
+     * 添加订单
+     * @param order 定单实例
+     */
     void addOrder(Order order);
 }
